0a8847925ef8ab70dec97923e4da59c83051ff09
DamiJ <damianojongerius@gmail.com>
2023-09-25 20:56:07 +0200

    worldGeneration

---
+++ Assets/scripts/World/Generation/WorldGenerator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace World.Generation
+{
+    public class WorldGenerator : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/scripts/World/WorldData/Chunk.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace World.WorldData
+{
+    public class Chunk : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
